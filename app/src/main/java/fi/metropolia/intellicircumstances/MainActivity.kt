fe6b37c24acc8d0d8075ff7ad02da071b2124fe4
Tiitus Telke <tiitus.telke@metropolia.fi>
2022-03-01 14:36:53 +0200

    Add app icon, splash screen

--- app/src/main/java/fi/metropolia/intellicircumstances/MainActivity.kt
+++ app/src/main/java/fi/metropolia/intellicircumstances/MainActivity.kt
@@ -3,11 +3,13 @@ package fi.metropolia.intellicircumstances
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import fi.metropolia.intellicircumstances.navigation.Navigation
 import fi.metropolia.intellicircumstances.ui.theme.IntelliCircumstancesTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val asd = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             IntelliCircumstancesTheme {
